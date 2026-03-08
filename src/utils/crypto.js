dafb17b10f583231a7227143b1909507b56d75df
bikwong68 <jaydensabngo10@gmail.com>
2026-03-08 19:53:36 +1100

    Delete src directory

--- src/utils/crypto.js
+++
@@ -1,6 +0,0 @@
-export async function hashPassword(password) {
-  const msgUint8 = new TextEncoder().encode(password);
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-}
